43888fc6ca0c86771b3b086d3437b4d90998089a
mchurbanov <7888051@gmail.com>
2018-10-14 19:23:53 +0300

    added form show

---
+++ script.js
@@ -0,0 +1,14 @@
+  $(document).ready(function() {
+	$(".show-form").fancybox({
+        padding     : 0,
+		maxWidth	: 417,
+		maxHeight	: 644,
+		fitToView	: false,
+		width		: '70%',
+		height		: '70%',
+		autoSize	: false,
+		closeClick	: false,
+		openEffect	: 'none',
+		closeEffect	: 'none'
+	});
+  });  
