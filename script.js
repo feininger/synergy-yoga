217a3ea038732b37dd18546ab27c6c51aab72862
mchurbanov <7888051@gmail.com>
2018-10-14 19:47:52 +0300

    added video popup

--- script.js
+++ script.js
@@ -27,3 +27,19 @@
 
 	});
   });
+
+  $(document).ready(function() {
+	$(".video").fancybox({
+        padding     : 0,
+		maxWidth	: 800,
+		maxHeight	: 600,
+		fitToView	: false,
+		width		: '70%',
+		height		: '70%',
+		autoSize	: false,
+		closeClick	: false,
+		openEffect	: 'none',
+		closeEffect	: 'none'
+
+	});
+  });  
